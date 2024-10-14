08ae7e4ce81a93ad2b8ce8c35a9544f6e257e936
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-10-14 17:38:59 +0500

    2

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React from 'react';
+import './App.css';
+const ProductsList = React.lazy(() => {
+	return import('./ProductList');
+});
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = React.useState(0);
+	const [showProducts, setShowProducts] = React.useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	function increment() {
+		setCount((prevCount) => prevCount + 1);
+	}
+
+	function decrement() {
+		setCount((prevCount) => prevCount - 1);
+	}
+
+	return (
+		<>
+			<h1>The current count is {count}</h1>
+			<button className='button' onClick={decrement}>
+				-
+			</button>
+			<button className='button' onClick={increment}>
+				+
+			</button>
+			<br />
+			<br />
+			<button
+				className='button'
+				onClick={() => setShowProducts((prev) => !prev)}
+			>
+				Show Products
+			</button>
+			<br />
+			<br />
+			<div className='products-list'>
+        {showProducts}
+        </div>
+
+		</>
+	);
 }
 
-export default App
+export default App;
